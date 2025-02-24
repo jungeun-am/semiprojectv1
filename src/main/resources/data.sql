d4591e836b0ed6b23515775ff2bd6d879f772e04
jungeun <jungeun0054@gmail.com>
2025-02-24 14:17:56 +0900

    fetch를 이용한 회원가입 기능 구현4 비밀번호 에 대해 SHA-256해시 생성 (수정)

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,4 +2,4 @@
 
 insert into members(userid, password, name, email) values
 ('abc123', '6ca13d52ca70c883e0f0bb101e425a89e8624de51db2d2392593af6a84118090', 'abc123', 'abc123@abc123.co.kr' ),
-('987xyz','987xyz','c7d0cccedf3900d458784c9fba1336d71c434bc380474cc219fda24a2d420f8f','987xyz@987xyz.co.kr');
+('987xyz','c7d0cccedf3900d458784c9fba1336d71c434bc380474cc219fda24a2d420f8f','987xyz','987xyz@987xyz.co.kr');
